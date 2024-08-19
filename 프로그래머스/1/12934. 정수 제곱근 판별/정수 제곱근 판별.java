200872d1c2bd98efdc040acbcc38d225abdb5778
kim-minkyoung <105150339+kim-minkyoung@users.noreply.github.com>
2024-08-19 19:15:08 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long sqrt = (long) Math.sqrt(n);
+        if (sqrt*sqrt == n) return (sqrt+1)*(sqrt+1);
+        return -1;
+    }
+    
+}
