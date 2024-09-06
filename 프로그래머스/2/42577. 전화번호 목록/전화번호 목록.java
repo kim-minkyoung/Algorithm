5b291844f97adc2d7e64388235a21dd6052921c6
kim-minkyoung <105150339+kim-minkyoung@users.noreply.github.com>
2024-09-06 14:30:41 +0900

    [level 2] Title: 전화번호 목록, Time: 258.60 ms, Memory: 209 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String phone: phone_book){
+            map.put(phone, 0);
+        }
+        for(String phone: phone_book){
+            for(int i=1; i<phone.length(); i++){
+                if(map.containsKey(phone.substring(0, i))) return false;
+        }
+        }
+        
+        return true;
+    }
+}
